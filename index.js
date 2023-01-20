6c0892964253e62b8aa5a1ae2f23e7bd8bd354fe
RCHcalvo <rchcalvo@example.com>
2023-01-20 17:05:13 -0600

    se agrega funcion de multiplicacion

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ function suma (num1 ,num2) {
 function resta (num3 , num4){
     return num3 - num4
 }
+function multiplicacion (num5 , num6){
+    return num5 * num6
+}
 let res = suma(3,5);
 console.log( res );
 
 res = resta(6,4);
 console.log(res);
+
+res = multiplicacion(8,9)
+console.log(res);
+
 
