16b9ee08b04c67ab7477a3e686d7a652f4966e6c
RCHcalvo <rchcalvo@example.com>
2023-01-20 16:55:10 -0600

    se agrega funcion de resta

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function suma (num1 ,num2) {
     return num1 + num2 
 }
+
+function resta (num3 , num4){
+    return num3 - num4
+}
 let res = suma(3,5);
 console.log( res );
+
+res = resta(6,4);
+console.log(res);
 
