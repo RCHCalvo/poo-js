c72eba8bf00a214d28199768a0f5ca97b85fa836
RCHCalvo <123213060+RCHCalvo@users.noreply.github.com>
2023-01-20 17:23:34 -0600

    se agrega funcion de division

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ function suma (num1 ,num2) {
 function resta (num3 , num4){
     return num3 - num4
 }
+
 function multiplicacion (num5 , num6){
     return num5 * num6
 }
+
+function division (num7 , num8){
+    return num7 / num8
+}
+
 let res = suma(3,5);
 console.log( res );
 
@@ -15,6 +21,9 @@ res = resta(6,4);
 console.log(res);
 
 res = multiplicacion(8,9)
+console.log(res);
+
+res = division (10, 2)
 console.log(res);
 
 
